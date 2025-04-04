c29e43a6f6ea8abde6bc550d8a0458022bb406c3
David Vieira <david.sousa.vieira@hotmail.com>
2025-04-04 11:34:13 +0100

    setup routing and starting smartbill screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,41 +1,30 @@
-import { View } from "react-native";
+import {
+  NavigationContainer,
+  NavigationIndependentTree,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import Button from "../Components/Buttons/Button-v1/Button";
+import HomeScreen from "./screens/HomeScreen";
+import SmartBill from "./screens/SmartBill";
+
+const Stack = createNativeStackNavigator();
 
 export default function Index() {
   return (
-    <SafeAreaProvider
-      style={[
-        {
-          padding: 20,
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#273C47",
-        },
-      ]}
-    >
-      <View style={{ width: "100%", gap: 10 }}>
-        <View>
-          <Button
-            title="Smart Bill"
-            onPress={() => console.log("Button clicked")}
-            variant="primary"
-          />
-        </View>
-        <View style={{ flex: 1, flexDirection: "row", gap: 10, width: "100%" }}>
-          <Button
-            title="Receitas"
-            onPress={() => console.log("Button clicked")}
-            variant="secondary"
-          />
-          <Button
-            title="Dividir Despesas"
-            onPress={() => console.log("Button clicked")}
-            variant="secondary"
-          />
-        </View>
-      </View>
+    // ...
+    <SafeAreaProvider>
+      <NavigationIndependentTree>
+        <NavigationContainer>
+          <Stack.Navigator
+          //  screenOptions={{
+          //    headerShown: false,
+          // }}
+          >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="SmartBill" component={SmartBill} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NavigationIndependentTree>
     </SafeAreaProvider>
   );
 }
---
+++ components/buttons/Button.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import styles from "./styleButton";
+
+interface ButtonProps {
+  title: string;
+  onPress: () => void;
+  variant?: "primary" | "secondary" | "alternative" | "disabled";
+}
+
+export default function Button({
+  title,
+  onPress,
+  variant = "primary",
+}: ButtonProps) {
+  const variantStyles = {
+    primary: styles.primary,
+    secondary: styles.secondary,
+    alternative: styles.alternative,
+    disabled: styles.disabled,
+  };
+
+  const buttonStyle = variantStyles[variant] || styles.primary;
+  const isDisabled = variant === "disabled";
+
+  return (
+    <TouchableOpacity
+      style={[styles.button, buttonStyle]}
+      onPress={onPress}
+      activeOpacity={isDisabled ? 1 : 0.7}
+      disabled={isDisabled}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
